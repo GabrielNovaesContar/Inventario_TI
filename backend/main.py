a943603068d706654983c8b947d12c449f3d8651
GabrielNovaesContar <gabrielnovaes@contarconsultoria.com>
2026-07-02 11:09:21 -0300

    feat: adicionando infraestrutura docker

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,21 @@
 import os
-
+import sys
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import init_db
+
+# Registra os roteadores
 from routers import auth, assets, parts, supplies
-import uvicorn
-from fastapi.staticfiles import StaticFiles
-import sys
 
 app = FastAPI(title="API Inventário TI")
 
+# =================================================================
+# CORREÇÃO DE SEGURANÇA (CORS) - Permite comunicação com o Nginx
+# =================================================================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -21,18 +24,13 @@ app.add_middleware(
 # Cria as tabelas ao iniciar
 init_db()
 
-# Registra as Rotas
+# Registra as Rotas da API
 app.include_router(auth.router)
 app.include_router(assets.router)
 app.include_router(parts.router)
 app.include_router(supplies.router)
 
-if getattr(sys, 'frozen', False):
-    frontend_dir = os.path.join(os.path.dirname(sys.executable), "frontend")
-else:
-    frontend_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "frontend")
-
-app.mount("/", StaticFiles(directory=frontend_dir, html=True), name="frontend")
+# REMOVIDO: O bloco do StaticFiles, pois o Nginx agora cuida do Frontend!
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
